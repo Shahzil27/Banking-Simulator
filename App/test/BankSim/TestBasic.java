6a569e18fe6f5d8a8c300b3e245de9ebe778a0bb
Shahzil Siddiqui <shahzil.s23@gmail.com>
2024-07-19 00:10:01 -0600

    Updated TestBasic: Removed resetCounter, LocalDate handle

--- App/test/BankSim/TestBasic.java
+++ App/test/BankSim/TestBasic.java
@@ -2,48 +2,67 @@ package BankSim;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import java.time.LocalDate;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 
+
+@TestInstance(Lifecycle.PER_CLASS)
 class TestBasic {
-
+	
+	AccountHolderInfo accountHolder;
+	Basic basicAcc;
+	
+	@BeforeAll
+	void initialize_parent() {
+		accountHolder = new AccountHolderInfo("BasicUser", "Pass123", "Basic", "User", "123 Basic Street", "basic123@gmail.com", "6391234567");
+	}
+	
+	@BeforeEach
+	void initialize_kidPlan() {
+		basicAcc = new Basic(accountHolder, "", "");
+	}
+	
 	@Test
 	void test_Constructor_AccountHolderInfo() {
-		
-		AccountHolderInfo accountHolder = new AccountHolderInfo("BasicUser", "Pass123", "Basic", "User", "123 Basic Street", "basic123@gmail.com", "6391234567"); 
-		Basic basicAcc = new Basic(accountHolder, "", "");
-		
 		assertEquals(accountHolder, basicAcc.getAccountHolderInfo()); 
 	}
 	
 	@Test
-	void test_Constructor_Chequing() {
-		
-		AccountHolderInfo accountHolder = new AccountHolderInfo("BasicUser", "Pass123", "Basic", "User", "123 Basic Street", "basic123@gmail.com", "6391234567"); 
-		Basic basicAcc = new Basic(accountHolder, "", "");
+	void test_Constructor_Chequing_Balance() {
 		Account chequingAcc = new Account ("chequing", "3478 4733 5684 2939", 2500.00f);  
 		
 		assertEquals(chequingAcc.getBalance(), basicAcc.getChequing().getBalance()); 
+	}
+	
+	@Test
+	void test_Constructor_Chequing_AccNum() {
+		Account chequingAcc = new Account ("chequing", "3478 4733 5684 2939", 2500.00f);  
+		
 		assertEquals(chequingAcc.getAccountNumber(), basicAcc.getChequing().getAccountNumber());
+	}
+	
+	@Test
+	void test_Constructor_Chequing_Type() {
+		Account chequingAcc = new Account ("chequing", "3478 4733 5684 2939", 2500.00f);  
+		
 		assertEquals(chequingAcc.getType(), basicAcc.getChequing().getType()); 
 	}
 	
 	@Test
-	void test_Constructor_publicVars() {
-		
-		AccountHolderInfo accountHolder = new AccountHolderInfo("BasicUser", "Pass123", "Basic", "User", "123 Basic Street", "basic123@gmail.com", "6391234567"); 
-		Basic basicAcc = new Basic(accountHolder, "", "");
-		LocalDate dateNow = LocalDate.now();   
-		
+	void test_Constructor_publicVars_WithdrawCount() {
 		assertEquals(0f, basicAcc.dailyWithdrawCount);
+	}
+	
+	@Test
+	void test_Constructor_publicVars_TransActionCount() {
 		assertEquals(0, basicAcc.dailyTransactionCount);
 	}
 	
 	@Test
 	void test_Withdraw_Success() {
-		
-		AccountHolderInfo accountHolder = new AccountHolderInfo("BasicUser", "Pass123", "Basic", "User", "123 Basic Street", "basic123@gmail.com", "6391234567"); 
-		Basic basicAcc = new Basic(accountHolder, "", "");
 		Tuple result = basicAcc.withdraw(100f, basicAcc.getChequing()); 
 		
 		assertEquals(true, result.getTaskStatus());
@@ -51,9 +70,6 @@ class TestBasic {
 	
 	@Test
 	void test_Withdraw_Failure_notEnoughBalance() {
-		
-		AccountHolderInfo accountHolder = new AccountHolderInfo("BasicUser", "Pass123", "Basic", "User", "123 Basic Street", "basic123@gmail.com", "6391234567"); 
-		Basic basicAcc = new Basic(accountHolder, "", "");
 		Tuple result = basicAcc.withdraw(3000f, basicAcc.getChequing()); 
 		
 		assertEquals(false, result.getTaskStatus());
@@ -61,9 +77,6 @@ class TestBasic {
 	
 	@Test
 	void test_Withdraw_Failure_exceedsWithdrawLimit() {
-		
-		AccountHolderInfo accountHolder = new AccountHolderInfo("BasicUser", "Pass123", "Basic", "User", "123 Basic Street", "basic123@gmail.com", "6391234567"); 
-		Basic basicAcc = new Basic(accountHolder, "", "");
 		Tuple result = basicAcc.withdraw(201f, basicAcc.getChequing()); 
 		
 		assertEquals(false, result.getTaskStatus());
@@ -71,10 +84,6 @@ class TestBasic {
 	
 	@Test
 	void test_Withdraw_Failure_exceedsTotalWithdrawLimit() {
-		
-		AccountHolderInfo accountHolder = new AccountHolderInfo("BasicUser", "Pass123", "Basic", "User", "123 Basic Street", "basic123@gmail.com", "6391234567"); 
-		Basic basicAcc = new Basic(accountHolder, "", "");
-		
 		basicAcc.withdraw(100f, basicAcc.getChequing()); 
 		Tuple result = basicAcc.withdraw(110f, basicAcc.getChequing());
 		
@@ -83,10 +92,6 @@ class TestBasic {
 	
 	@Test
 	void test_WithdrawBoundary_Success_DailyTransactionCount() {
-		
-		AccountHolderInfo accountHolder = new AccountHolderInfo("BasicUser", "Pass123", "Basic", "User", "123 Basic Street", "basic123@gmail.com", "6391234567"); 
-		Basic basicAcc = new Basic(accountHolder, "", "");
-		
 		basicAcc.withdraw(10f, basicAcc.getChequing());
 		basicAcc.withdraw(10f, basicAcc.getChequing());
 		basicAcc.withdraw(10f, basicAcc.getChequing());
@@ -103,10 +108,6 @@ class TestBasic {
 	
 	@Test
 	void test_Withdraw_Failure_ExceedsDailyTransactionCount() {
-		
-		AccountHolderInfo accountHolder = new AccountHolderInfo("BasicUser", "Pass123", "Basic", "User", "123 Basic Street", "basic123@gmail.com", "6391234567"); 
-		Basic basicAcc = new Basic(accountHolder, "", "");
-		
 		basicAcc.withdraw(10f, basicAcc.getChequing());
 		basicAcc.withdraw(10f, basicAcc.getChequing());
 		basicAcc.withdraw(10f, basicAcc.getChequing());
@@ -124,9 +125,6 @@ class TestBasic {
 	
 	@Test
 	void test_Deposit_Success() {
-		
-		AccountHolderInfo accountHolder = new AccountHolderInfo("BasicUser", "Pass123", "Basic", "User", "123 Basic Street", "basic123@gmail.com", "6391234567"); 
-		Basic basicAcc = new Basic(accountHolder, "", "");
 		Tuple result = basicAcc.deposit(300f, basicAcc.getSavings()); 
 		
 		assertEquals(true, result.getTaskStatus());
@@ -134,10 +132,6 @@ class TestBasic {
 	
 	@Test
 	void test_DepositBoundary_Success_() {
-		
-		AccountHolderInfo accountHolder = new AccountHolderInfo("BasicUser", "Pass123", "Basic", "User", "123 Basic Street", "basic123@gmail.com", "6391234567"); 
-		Basic basicAcc = new Basic(accountHolder, "", "");
-		
 		basicAcc.deposit(300f, basicAcc.getSavings());
 		basicAcc.deposit(300f, basicAcc.getSavings());
 		basicAcc.deposit(300f, basicAcc.getSavings());
@@ -154,10 +148,6 @@ class TestBasic {
 	
 	@Test
 	void test_Deposit_Failure_ExceedsTransctionLimit() {
-		
-		AccountHolderInfo accountHolder = new AccountHolderInfo("BasicUser", "Pass123", "Basic", "User", "123 Basic Street", "basic123@gmail.com", "6391234567"); 
-		Basic basicAcc = new Basic(accountHolder, "", "");
-		
 		basicAcc.deposit(300f, basicAcc.getSavings());
 		basicAcc.deposit(300f, basicAcc.getSavings());
 		basicAcc.deposit(300f, basicAcc.getSavings());
@@ -175,9 +165,6 @@ class TestBasic {
 	
 	@Test
 	void test_ViewBalance_Success_Chequing() {
-		
-		AccountHolderInfo accountHolder = new AccountHolderInfo("BasicUser", "Pass123", "Basic", "User", "123 Basic Street", "basic123@gmail.com", "6391234567"); 
-		Basic basicAcc = new Basic(accountHolder, "", "");
 		float result = basicAcc.viewBalance(basicAcc.getChequing()); 
 		
 		assertEquals(2500.00f, result);
@@ -185,9 +172,6 @@ class TestBasic {
 	
 	@Test
 	void test_ViewBalance_Success_Savings() {
-		
-		AccountHolderInfo accountHolder = new AccountHolderInfo("BasicUser", "Pass123", "Basic", "User", "123 Basic Street", "basic123@gmail.com", "6391234567"); 
-		Basic basicAcc = new Basic(accountHolder, "", "");
 		float result = basicAcc.viewBalance(basicAcc.getSavings()); 
 		
 		assertEquals(500.00f, result);
@@ -195,9 +179,6 @@ class TestBasic {
 	
 	@Test
 	void test_TransferFunds_Success() {
-		
-		AccountHolderInfo accountHolder = new AccountHolderInfo("BasicUser", "Pass123", "Basic", "User", "123 Basic Street", "basic123@gmail.com", "6391234567"); 
-		Basic basicAcc = new Basic(accountHolder, "", "");
 		Tuple result = basicAcc.transferFunds(basicAcc.getChequing(), basicAcc.getSavings(), 50f);  
 		
 		assertEquals(true, result.getTaskStatus());
@@ -205,10 +186,6 @@ class TestBasic {
 	
 	@Test
 	void test_TransferFundsBoundary_Success() {
-		
-		AccountHolderInfo accountHolder = new AccountHolderInfo("BasicUser", "Pass123", "Basic", "User", "123 Basic Street", "basic123@gmail.com", "6391234567"); 
-		Basic basicAcc = new Basic(accountHolder, "", "");
-		
 		basicAcc.withdraw(10f, basicAcc.getChequing());
 		basicAcc.deposit(10f, basicAcc.getChequing());
 		basicAcc.withdraw(10f, basicAcc.getChequing());
@@ -226,55 +203,24 @@ class TestBasic {
 	@Test
 	void test_TransferFunds_Failure_ExceedsTransactionLimit() {
 		
-		AccountHolderInfo accountHolder = new AccountHolderInfo("BasicUser", "Pass123", "Basic", "User", "123 Basic Street", "basic123@gmail.com", "6391234567"); 
-		Basic basicAcc = new Basic(accountHolder, "", "");
-		System.out.println(basicAcc.getChequing().getBalance()); 
-		
 		basicAcc.withdraw(10f, basicAcc.getChequing());
-		System.out.println(basicAcc.getChequing().getBalance());
-		System.out.println(basicAcc.dailyTransactionCount); 
-		
 		basicAcc.deposit(10f, basicAcc.getChequing());
-		System.out.println(basicAcc.getChequing().getBalance());
-		System.out.println(basicAcc.dailyTransactionCount); 
-		
 		basicAcc.withdraw(10f, basicAcc.getChequing());
-		System.out.println(basicAcc.getChequing().getBalance());
-		System.out.println(basicAcc.dailyTransactionCount); 
-		
-		Tuple x = basicAcc.withdraw(10f, basicAcc.getChequing());
-		System.out.println(x.getResultsDescription()); 
-		System.out.println(basicAcc.dailyTransactionCount); 
-		
+		basicAcc.withdraw(10f, basicAcc.getChequing());
 		basicAcc.deposit(10f, basicAcc.getChequing());
-		System.out.println(basicAcc.dailyTransactionCount); 
-		
 		basicAcc.withdraw(10f, basicAcc.getChequing());
-		System.out.println(basicAcc.dailyTransactionCount); 
-		
 		basicAcc.withdraw(10f, basicAcc.getChequing());
-		System.out.println(basicAcc.dailyTransactionCount); 
-		
 		basicAcc.deposit(10f, basicAcc.getChequing());
-		System.out.println(basicAcc.dailyTransactionCount); 
-		
 		basicAcc.withdraw(10f, basicAcc.getChequing());
-		System.out.println(basicAcc.dailyTransactionCount); 
-		
 		basicAcc.withdraw(10f, basicAcc.getChequing());
-		System.out.println(basicAcc.dailyTransactionCount); 
 		
 		Tuple result = basicAcc.transferFunds(basicAcc.getChequing(), basicAcc.getSavings(), 50f);
-		System.out.println(basicAcc.dailyTransactionCount); 
 		
 		assertEquals(false, result.getTaskStatus());
 	}
 	
 	@Test
 	void test_TransferFunds_Failure_exceedsFunds() {
-		
-		AccountHolderInfo accountHolder = new AccountHolderInfo("BasicUser", "Pass123", "Basic", "User", "123 Basic Street", "basic123@gmail.com", "6391234567"); 
-		Basic basicAcc = new Basic(accountHolder, "", "");
 		Tuple result = basicAcc.transferFunds(basicAcc.getChequing(), basicAcc.getSavings(), 5000f);  
 		
 		assertEquals(false, result.getTaskStatus());
